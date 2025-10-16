e5bac80d62d8550a53faed4480265c6e233a8cdb
Leroi Kakatsi <kingiscoding@gmail.com>
2025-10-16 11:22:33 +0100

    refactor: request file and rules for registration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRegistrationRequest;
+use Exception;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    /**
+     * Register a user if data is valid
+     */
+    public function register(UserRegistrationRequest $request){
+        try{
+            
+            $validatedFields = $request->validated();
+            return response()->json(['data' => $validatedFields], 201);
+
+        } catch(Exception $ex){
+            $validatedFields = $request->validated();
+            return response()->json([$ex, $validatedFields], 401);
+        }
+    }
+
+
+    /**
+     * Login a user if data is valid
+     */
+    public function login(){
+        return response()->json(['test' => 'succeed'], 201);
+    }
+
+
     /**
      * Display a listing of the resource.
      */
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+
+Route::get('/modules', [UserController::class, 'register'])->middleware('auth:sanctum');
+
+Route::post('/modules/:id/activate', [UserController::class, 'register'])->middleware('auth:sanctum');
+Route::post('/modules/:id/deactivate', [UserController::class, 'register'])->middleware('auth:sanctum');
+// Route::get('/register', [UserController::class, 'register']);
