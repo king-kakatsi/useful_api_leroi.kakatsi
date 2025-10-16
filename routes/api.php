a8ad22fc09de7ab7d3a56ddb15138f019002741d
Leroi Kakatsi <kingiscoding@gmail.com>
2025-10-16 15:12:59 +0100

    feat: check module action middleware implemented

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ModuleController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\CheckModuleActive;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [UserController::class, 'register']);
@@ -11,3 +12,4 @@ Route::get('/modules', [ModuleController::class, 'index'])->middleware('auth:san
 
 Route::post('/modules/{id}/activate', [UserController::class, 'activateUserModule'])->middleware('auth:sanctum');
 Route::post('/modules/{id}/deactivate', [UserController::class, 'deactivateUserModule'])->middleware('auth:sanctum');
+// Route::post('/modules/{id}/deactivate', [UserController::class, 'deactivateUserModule'])->middleware('auth:sanctum', CheckModuleActive::class);
